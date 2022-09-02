4d371674f4b736a01dede1500cd9ed0488d51573
Chinedum <chinedumike85@gmail.com>
2022-09-02 14:06:03 +0100

    Tourist advisor app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 
 import {CssBaseline, Grid} from '@material-ui/core'
 
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
-import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+import { getPlacesData, getWeatherData } from './api'
 
-function App(){
+function App(){ 
+    const [places, setPlaces] =  useState([])
+    const [coordinates, setCoordinates] = useState({})
+    const [bounds, setBounds] = useState({})
+    const [childClicked, setChildClicked] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
+    const [filteredPlaces, setFilteredPlaces] = useState([])  //when the user changes the rating, filter the places array according to the rating
+    const [weatherData, setWeatherData] = useState([])
+    
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
+            setCoordinates({lat: latitude, lng: longitude})
+        })
+    },[])
+
+    useEffect(()=> {
+            const filteredPlaces = places.filter((place)=> place.rating > rating )
+            setFilteredPlaces(filteredPlaces)
+    }, [rating])
+
+    useEffect(()=>{
+        if(bounds.sw && bounds.ne){
+            setIsLoading(true)
+
+            // getWeatherData(coordinates.lat, coordinates.lng)
+            //     .then((data) => setWeatherData(data));
+
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data)=>{
+                    setPlaces(data.filter((place) => place.name && place.num_reviews > 0))
+                    setFilteredPlaces([])
+                    setIsLoading(false)
+                })
+        }
+    },[type, bounds])
+
     return(
         <>
             <CssBaseline/>
-            <Header/>
+            <Header setCoordinates={setCoordinates}/>
             <Grid container spacing = {3} style ={{ width: '100%'}}>
                 <Grid item xs ={12} md={4}>
-                    <List/>
+                    <List 
+                        places ={filteredPlaces.length ? filteredPlaces : places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
                 </Grid>
                 <Grid item xs ={12} md={8}>
-                    <Map/>
+                    <Map 
+                        coordinates={coordinates}
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked = {setChildClicked}
+                        weatherData={weatherData}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import {React, useState , useEffect, createRef} from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
-const List = () =>{
+import useStyles from './style'
+const List = ({places, childClicked, isLoading, type, setType, rating, setRating}) =>{
+    const classes = useStyles()
+    const [elRefs, setElRefs] = useState([])
+
+    //creating a ref for every single item in the places array and equating it into the elRefs
+    useEffect(() => {
+        const refs = Array(places?.length).fill().map((_, index) => elRefs[index] || createRef())
+        setElRefs(refs);
+      }, [places]);
+
     return(
-        <div>
-            <h1>List</h1>
+        <div className={classes.container}>
+            <Typography variant='h4'>Restaurants, Hotels and Attractions</Typography>
+            {isLoading ? (
+                <div className={classes.loading}>
+                    <CircularProgress/>
+                </div>
+            ): (<>
+            <FormControl className={classes.formControl} >
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange ={(e) => setType(e.target.value)} >
+                    <MenuItem value='restaurants'>Restaurants</MenuItem>
+                    <MenuItem value='hotels'>Hotels</MenuItem>
+                    <MenuItem value='attractions'>Attractions</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl} >
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange ={(e) => setRating(e.target.value)} >
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, index) => (
+                    //passing the refs as props
+                    //giving the place details a property of selected if the childclicked and index are the same
+                    <Grid ref={elRefs[index]} item key={index} xs={12}>
+                        <PlaceDetails 
+                            place={place}
+                            selected={Number(childClicked) === index}
+                            refProp={elRefs[index]}
+                        />
+
+                    </Grid>
+                ))}
+            </Grid>
+            </>)}
         </div>
     )
 }
 export default List
+
